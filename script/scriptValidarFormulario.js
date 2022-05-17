346421dc789c49e7085516464e6add1041983f90
Vinicius Ricci <32933550+ViniciusRicciChamps@users.noreply.github.com>
2022-05-17 14:54:42 -0300

    Criação de um script para capturar o valores do input

---
+++ script/scriptValidarFormulario.js
@@ -0,0 +1,9 @@
+
+var botaoValidarForm = document.getElementById("botaoSubmitFormulario").addEventListener("click",
+    function capturarValoresFormulario() {
+        //console.log("Botao funcionando");
+
+        var primeiroNomeUsuario = document.getElementById("primeiroNomeUsuarioFormulario").value;
+
+
+    })
